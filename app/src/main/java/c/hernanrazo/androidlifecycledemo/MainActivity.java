c844d92af73fe2fd805aa189b2c62ddbae6c9eea
Hernan Razo <hernanrazo@gmail.com>
2018-06-01 09:24:34 -0500

    added more callbacks and added to README.md

--- app/src/main/java/c/hernanrazo/androidlifecycledemo/MainActivity.java
+++ app/src/main/java/c/hernanrazo/androidlifecycledemo/MainActivity.java
@@ -5,9 +5,43 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    //called when activity is first created
+    // this callback is mandatory
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
         setContentView(R.layout.activity_main);
     }
 }
