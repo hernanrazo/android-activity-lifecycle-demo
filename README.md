1363bf087dc638acc5bfe1daae088444d0ad6f5e
Hernan Razo <hernanrazo@gmail.com>
2018-06-01 14:14:00 -0500

    Added one more activity and button to switch between the two

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-An android app demo that showcases the android activity lifecycle.
+An android app demo that showcases the android activity lifecycle. There are two activities, MainActivity and secondActivity. Using the logcat and log class, this app shows the user when each callback is called in both activities. Both activities have a button that is used to switch between the two and showcase how the lifecycles interact in each respective activity and when switching between the two. 
+
+##onCreate()
+##onStart()
+##onResume()
+##onPause()
+##onStop()
+##onDestroy()
+
+
+
+
+
+Sources and helpful links:
+
+https://developer.android.com/guide/components/activities/activity-lifecycle#java
--- app/src/main/java/c/hernanrazo/androidlifecycledemo/secondActivity.java
+++ app/src/main/java/c/hernanrazo/androidlifecycledemo/secondActivity.java
@@ -3,59 +3,53 @@ package c.hernanrazo.androidlifecycledemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class secondActivity extends AppCompatActivity implements View.OnClickListener {
 
     //setup variables
-    private static final String TAG = "MainActivityTag";
+    private static final String TAG = "secondActivityTag";
 
-    // set logic for button
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
 
             case R.id.button:
                 //add logic for when the button is pressed
-                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(secondActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
                 Log.i(TAG,"button pressed");
 
-                Intent secondActivityIntent = new Intent(MainActivity.this, secondActivity.class);
-                startActivity(secondActivityIntent);
+                Intent MainActivityIntent = new Intent(secondActivity.this, MainActivity.class);
+                startActivity(MainActivityIntent);
         }
     }
 
 
     //========================================activity lifecycle callbacks code===============================
 
+
     //called when activity is first created
     // this callback is mandatory
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         Log.i(TAG, "onCreate() callback started");
 
         //Set up UI components in this callback in order to have them at the start of the activity
 
-        //initiate clickable link for my github account
-        TextView githubLink = findViewById(R.id.gitHub);
-        githubLink.setMovementMethod(LinkMovementMethod.getInstance());
-
         //set listener for button
-       final Button button = findViewById(R.id.button);
-       button.setOnClickListener(this);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         Log.i(TAG, "onStart() callback started");
 
     }
@@ -63,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         Log.i(TAG, "onResume() callback started");
 
     }
@@ -71,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onPause() {
         super.onPause();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         Log.i(TAG, "onPause() callback started");
 
     }
@@ -79,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStop() {
         super.onStop();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         Log.i(TAG, "onStop() callback started");
 
     }
@@ -87,7 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         Log.i(TAG, "onDestroy() callback started");
     }
 }
